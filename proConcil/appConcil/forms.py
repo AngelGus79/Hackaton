020492270d852683d5c0186032827a584b5ab8e4
AlonsoIbarra <saul.ibarra@cimat.mx>
2017-06-24 17:13:29 -0500

    Se agrego formato de bootstrap a página de calcular

--- proConcil/appConcil/forms.py
+++ proConcil/appConcil/forms.py
@@ -6,3 +6,10 @@ class formLey(forms.ModelForm):
     class Meta:
         model = modelLey
         fields = "__all__"
+        widgets = {
+            'Ag': forms.TextInput(attrs={'class': 'form-control'}),
+            'Av': forms.TextInput(attrs={'class': 'form-control'}),
+            'Pd': forms.TextInput(attrs={'class': 'form-control'}),
+            'Zn': forms.TextInput(attrs={'class': 'form-control'}),
+            'Cu': forms.TextInput(attrs={'class': 'form-control'}),
+        }
